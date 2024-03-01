584dacdaa8f5395f19c4875a59b34a1c4ba5964a
LUCPHAN <lucptt.22it@vku.udn.vn>
2024-03-01 11:38:15 +0700

    add: database setup func

---
+++ database/databasetup.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DBset() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/ecommerce?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Susscessfully connected to MYSQL")
+	return db
+}
