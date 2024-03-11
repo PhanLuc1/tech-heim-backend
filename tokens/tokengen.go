88ebaf39c686750b99af6fafb43e4b8d9ceb4049
LUCPHAN <lucptt.22it@vku.udn.vn>
2024-03-12 06:16:04 +0700

    feat: add generatoken func

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,8 +1,41 @@
 package tokens
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type SignedDetails struct {
 	User_id int
 	jwt.StandardClaims
+}
+
+var SECRET_KEY = os.Getenv("SECRET_LOVE")
+
+func TokenGeneration(userid int) (signedtoken string, signedrefreshtoken string, err error) {
+	claims := &SignedDetails{
+		User_id: userid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(1)).Unix(),
+		},
+	}
+	refreshclaims := &SignedDetails{
+		User_id: userid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+	refreshtoken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshclaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+	return token, refreshtoken, err
 }
