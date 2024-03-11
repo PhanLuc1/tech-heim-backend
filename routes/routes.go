481c97035f992497a55f60b24bf8361883719d86
LUCPHAN <lucptt.22it@vku.udn.vn>
2024-03-12 00:32:46 +0700

    feat: add UserRoutes

---
+++ routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/PhanLuc1/tech-heim-backend/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/user/signup", controller.Signup())
+}
