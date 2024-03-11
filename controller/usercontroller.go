05ce78ccbeb6386ae8e5d7aee032c18512ecdfc3
LUCPHAN <lucptt.22it@vku.udn.vn>
2024-03-12 06:33:12 +0700

    feat: add Login func

--- controller/usercontroller.go
+++ controller/usercontroller.go
@@ -2,13 +2,17 @@ package controller
 
 import (
 	"log"
+	"net/http"
 	"regexp"
 
 	"github.com/PhanLuc1/tech-heim-backend/database"
 	"github.com/PhanLuc1/tech-heim-backend/models"
+	generate "github.com/PhanLuc1/tech-heim-backend/tokens"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var refreshTokenMap = make(map[string]bool)
 
 func HashPassword(password string) string {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 15)
@@ -16,6 +20,16 @@ func HashPassword(password string) string {
 		log.Panic(err)
 	}
 	return string(bytes)
+}
+func VerifyPassword(userpassword string, givenpassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(givenpassword), []byte(userpassword))
+	valid := true
+	msg := ""
+	if err != nil {
+		msg = "Login Or Passowrd is Incorerct"
+		valid = false
+	}
+	return valid, msg
 }
 func Signup() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -39,5 +53,34 @@ func Signup() gin.HandlerFunc {
 			return
 		}
 		ctx.JSON(201, gin.H{"message": "Your account has been created"})
+	}
+}
+func Login() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var user models.User
+		var founduser models.User
+		if err := ctx.BindJSON(&user); err != nil {
+			ctx.JSON(400, gin.H{"message": err.Error()})
+			return
+		}
+		err := database.Client.QueryRow("SELECT * FROM user WHERE email = ?", user.Email).Scan(
+			&founduser.Id,
+			&founduser.Email,
+			&founduser.First_Name,
+			&founduser.Last_Name,
+			&founduser.Password,
+		)
+		if err != nil {
+			ctx.JSON(500, gin.H{"Message": "Email is not available"})
+			return
+		}
+		PasswordIsValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+		if !PasswordIsValid {
+			ctx.JSON(401, gin.H{"error": msg})
+			return
+		}
+		token, refreshToken, _ := generate.TokenGeneration(*founduser.Id)
+		refreshTokenMap[refreshToken] = true
+		ctx.IndentedJSON(http.StatusOK, gin.H{"token": token, "refreshToken": refreshToken})
 	}
 }
