20318d1ef4ff0dd88420fc7eb7c40b52a6ad3488
LUCPHAN <lucptt.22it@vku.udn.vn>
2024-03-13 17:01:40 +0700

    setup: setup route product

---
+++ routes/productroutes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/PhanLuc1/tech-heim-backend/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func ProductRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/product", controller.GetProduct())
+}
